c723aecd42dd37d6f1d533e652e0a29bb790fe23
Thibaut chevée <tchevee@hotmail.fr>
2025-03-06 11:04:42 +0100

    feat: expand generation() to handle mutliple ids

--- identity/generation.py
+++ identity/generation.py
@@ -5,6 +5,9 @@ import random
 from .constants import ID_CHARACTERS
 
 
-def generate():
-    id = random.choices(ID_CHARACTERS, k=7)
-    return "".join(id)
+def generate(n=1):
+    new_ids = set()
+    while len(new_ids) < n:
+        new_id = "".join(random.choices(ID_CHARACTERS, k=7))
+        new_ids.add(new_id)
+    return list(new_ids)
