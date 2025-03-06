f23e235953ac0fa6f0cf457b49de3d3db853da37
Thibaut chevée <tchevee@hotmail.fr>
2025-03-06 11:09:36 +0100

    test: adapt to bulk generation, check types. new tests: test_generate_proper_type, test_bulk_generation

--- tests/test_generation.py
+++ tests/test_generation.py
@@ -7,18 +7,27 @@ from identity.constants import ID_CHARACTERS
 
 
 def test_id_is_the_correct_length():
-    assert len(generate()) == 7
+    assert len(generate()[0]) == 7
+
+
+def test_generate_proper_type():
+    assert type(generate()[0]) is str
+    assert type(generate()) is list
 
 
 def test_id_uses_correct_characters():
     generated_id = generate()
     check = re.compile('^[' + ID_CHARACTERS + ']+$')
-    assert check.match(generated_id) is not None
+    assert check.match(generated_id[0]) is not None
 
 
 def test_ids_are_unique():
     ids = set()
-    for i in range(0, 10):
-        ids.add(generate())
+    for i in range(0, 1000000):
+        ids.add(generate()[0])
+    assert len(ids) == 1000000
 
-    assert len(ids) == 10
+
+def test_bulk_generation():
+    ids = generate(10000)
+    assert len(ids) == 10000
