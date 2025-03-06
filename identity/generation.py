f6abbc3403fb9382b1517ae3c6fc80377543f931
Thibaut chevée <tchevee@hotmail.fr>
2025-03-06 09:51:04 +0100

    feat: core random generation function

--- identity/generation.py
+++ identity/generation.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import random
+
 from .constants import ID_CHARACTERS
 
 
 def generate():
-    return None
+    id = random.choices(ID_CHARACTERS, k=7)
+    return "".join(id)
