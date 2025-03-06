1793f2417afcc7859daf6dabb3b3b940fc3ece12
Thibaut chevée <tchevee@hotmail.fr>
2025-03-06 14:48:13 +0100

    feat: separate generate() in two functions (after checking the other branchs)

--- identity/generation.py
+++ identity/generation.py
@@ -5,9 +5,13 @@ import random
 from .constants import ID_CHARACTERS
 
 
-def generate(n=1):
+def generate():
+    return "".join(random.choices(ID_CHARACTERS, k=7))
+
+
+def generate_bulk(n=1):
     new_ids = set()
     while len(new_ids) < n:
-        new_id = "".join(random.choices(ID_CHARACTERS, k=7))
+        new_id = generate()
         new_ids.add(new_id)
     return list(new_ids)
--- tests/test_generation.py
+++ tests/test_generation.py
@@ -2,43 +2,40 @@
 
 import re
 import pytest
-from identity.generation import generate
+from identity.generation import generate, generate_bulk
 from identity.constants import ID_CHARACTERS
 
 format_check = re.compile('^[' + ID_CHARACTERS + ']{7}$')
 
 
 def test_id_is_the_correct_length():
-    assert len(generate()[0]) == 7
+    assert len(generate()) == 7
 
 
 def test_generate_proper_type():
-    assert type(generate()[0]) is str
-    assert type(generate()) is list
+    assert type(generate()) is str
+    assert type(generate_bulk()) is list
+    assert type(generate_bulk()[0]) is str
 
 
 def test_id_uses_correct_characters():
     generated_id = generate()
-    check = re.compile('^[' + ID_CHARACTERS + ']+$')
-    assert check.match(generated_id[0]) is not None
+    assert format_check.match(generated_id) is not None
 
 
 def test_ids_are_unique_and_correct_format():
     ids = set()
-    for i in range(0, 10000):
-        ids.add(generate()[0])
-    assert len(ids) == 10000
     generated_count = 0
     while generated_count < 22000:
         generated_id = generate()
         generated_count += 1
-        assert generated_id[0] not in ids
-        ids.add(generated_id[0])
-        assert format_check.match(generated_id[0]) is not None
+        assert generated_id not in ids
+        ids.add(generated_id)
+        assert format_check.match(generated_id) is not None
 
 
 def test_bulk_generation():
-    ids = generate(10000)
+    ids = generate_bulk(10000)
     assert len(ids) == 10000
 
 
@@ -46,6 +43,6 @@ def test_ids_are_unique_generated_in_bulk():
     generated_ids = set()
     generated_count = 0
     while generated_count < 100:
-        generated_ids.update(generate(1000))
+        generated_ids.update(generate_bulk(1000))
         generated_count += 1
         assert len(generated_ids) == generated_count * 1000
